bd20c04286febb1257c3d1ca8a86bd47a37e9ece
T <tjbergstrom@alaska.edu>
2020-04-28 17:43:41 -0800

    edits

--- Reverse Image Search/index.py
+++ Reverse Image Search/index.py
@@ -1,14 +1,8 @@
+# make an index of feature vectors of every photo in a dataset
+# the search uses that index to find images
 
-
-# run: python3 index.py --dataset dataset --index index.csv
-
-# run this file to set up an index of images to search thru
-# this will take all photos from your dataset and save 
-# each filename with its feature vector in the index.csv file
-
-# need to have a bunch of photos saved in the folder dataset
-# open up the index.csv file to see that the feature vectors were saved
-
+# python3 index.py
+# python3 index.py -d photos -i index.csv
 
 
 from pyimagesearch.colordescriptor import ColorDescriptor
@@ -17,27 +11,21 @@ import glob
 import cv2
 
 ap = argparse.ArgumentParser()
-ap.add_argument("-d", "--dataset", required = True,
-help = "Path to the directory that contains the images to be indexed")
-ap.add_argument("-i", "--index", required = True,
-help = "Path to where the computed index will be stored")
+ap.add_argument("-d", "--dataset", type=str, default="photos")
+ap.add_argument("-i", "--index", type=str, default="index.csv")
 args = vars(ap.parse_args())
 
 cd = ColorDescriptor((8, 12, 3))
 output = open(args["index"], "w")
 
-# use glob to grab the image paths and loop over them
 for imagePath in glob.glob(args["dataset"] + "/*.jpg"):
-	# extract the image filename) 
 	imageID = imagePath[imagePath.rfind("/") + 1:]
 	image = cv2.imread(imagePath)
 	features = cd.describe(image)
-	# write the features to file
 	features = [str(f) for f in features]
 	output.write("%s,%s\n" % (imageID, ",".join(features)))
 
 output.close()
-
 
 
 
--- Reverse Image Search/search.py
+++ Reverse Image Search/search.py
@@ -1,69 +1,54 @@
+# upload an image and search for it
+# display the best match and its location
+# also display up to 9 similar images
 
+# python3 search.py -u uploads/img.jpg
+# python3 search.py -u uploads/img.jpg -i index.csv -r dataset
 
-# run: python3 search.py --index index.csv --query queries/filename.jpg --result-path dataset
-
-# run this file to actually search for similar images
-# --index is path to the index.csv file containing all feature vectors
-# --query is path to the image file you want to upload
-# --result-path is path to the found photos in the dataset
-
-# need to have the photo filename.jpg in the queries folder
-
-
-
-# this is everything you need to run:
-# (these are the linux commands)
-
-# sudo apt install python3-numpy
-# sudo apt-get install python3-opencv
-# sudo apt install python3-pip
-# sudo apt install python-pip
-# sudo pip3 install imutils
-
-
+# -u = path to the image you want to upload
+# -i = path to the csv index containing all feature vectors
+# -r = path to the found photos in the dataset
 
 from pyimagesearch.colordescriptor import ColorDescriptor
 from pyimagesearch.searcher import Searcher
+from imutils import build_montages
 import argparse
 import cv2
 
 ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--index", required = True,
-help = "Path to where the computed index will be stored")
-ap.add_argument("-q", "--query", required = True,
-help = "Path to the query image")
-ap.add_argument("-r", "--result-path", required = True,
-help = "Path to the result path")
+ap.add_argument("-r", "--result-path", type=str, default="photos")
+ap.add_argument("-i", "--index", type=str, default="index.csv")
+ap.add_argument("-u", "--upload", required = True)
 args = vars(ap.parse_args())
+
 cd = ColorDescriptor((8, 12, 3))
-
-# load the query image
-query = cv2.imread(args["query"])
-features = cd.describe(query)
-
-# perform the search
+upload = cv2.imread(args["upload"])
+features = cd.describe(upload)
 searcher = Searcher(args["index"])
 results = searcher.search(features)
 
-# display the query
-#cv2.imshow("Query", query)
-
-# loop over the results
 i = 0
+locs = []
+similars = []
+print(len(results))
 for (score, resultID) in results:
-	result = cv2.imread(args["result_path"] + "/" + resultID)
-	#cv2.imshow("Result", result)
-	#cv2.waitKey(0)
-	if(i<4):
-		print(args["result_path"] + "/" + resultID)
+	location = args["result_path"] + "/" + resultID
+	result = cv2.imread(location)
+	if(i<1):
+		title = "Best Match: " + location
 		im = cv2.resize(result, (600, 400))
-		cv2.imshow("Result", im)
+		cv2.imshow(title, im)
 		cv2.waitKey(0)
-	i += 1
+		i = 1
+	else:
+		similars.append(result)
+		locs.append(location)
 
 
-
-
+montage = build_montages(similars, (128, 128), (3, 3))[0]
+cv2.imshow("Similar Images", montage)
+print(locs)
+cv2.waitKey(0)
 
 
 
