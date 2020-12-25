43702e2f6c499ce2eec9148f9dc5a8676ba4a24b
T <tjbergstrom@alaska.edu>
2020-12-24 16:58:40 -0900

    refactor

--- Ologn Reverse Image Search/hashing.py
+++ Ologn Reverse Image Search/hashing.py
@@ -1,28 +1,32 @@
 # hashing.py
-# June 22, 2020
-# these are hashing operations
+# June 2020
+# Some hashing operations
 
 
 import numpy as np
 import cv2
 
-def hash_distance(image, hashSize=8):
-	# pre-process the image
-	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-	resized = cv2.resize(gray, (hashSize + 1, hashSize))
-	# compute the horizontal gradient between adjacent column pixels
-	diff = resized[:, 1:] > resized[:, :-1]
-	# convert the difference image to a hash
+
+def hash_distance(img_path, hash_size=16):
+	""" Convert an image to a hash """
+	img = cv2.imread(img_path)
+	img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+	img = cv2.resize(img, (hash_size + 1, hash_size))
+	# The horizontal gradients between adjacent column pixels
+	diff = img[:, 1:] > img[:, :-1]
+	# Convert the difference to a hash
 	return sum([2 ** i for (i, v) in enumerate(diff.flatten()) if v])
 
 
-# convert the hash to a NumPy 64-bit float and then back to int
 def convert_hash(h):
+	""" Get the int table index from a NumPy 64-bit float hash """
 	return int(np.array(h, dtype="float64"))
 
 
-# compute the Hamming distance between integers
 def hamming(a, b):
+	""" To compute the Hamming distance between 2 integers """
 	return bin(int(a) ^ int(b)).count("1")
 
 
+
+##
--- Ologn Reverse Image Search/search.py
+++ Ologn Reverse Image Search/search.py
@@ -1,60 +1,79 @@
 # search.py
-# June 22, 2020
-# upload a photo and find it in the directory
-# also find some similar photo
-# arg -d is the hash distance, increase it to find more similar photos
-
-# python3 search.py -u uploads/img_01.jpg
+# June 2020
+# Upload a photo and find the best match in the index
+# Also find some similar photos
+#
+# $ python3 search.py -u uploads/img_01.jpg
 
 
 from imutils import build_montages
 from hashing import hash_distance
 from hashing import convert_hash
 import argparse
+import imutils
 import pickle
 import time
 import cv2
-
-ap = argparse.ArgumentParser()
-ap.add_argument("-u", "--upload", required=True, type=str)
-ap.add_argument("-d", "--distance", type=int, default=10)
-args = vars(ap.parse_args())
-
-tree = pickle.loads(open("vptree.pickle", "rb").read())
-hashes = pickle.loads(open("hashes.pickle", "rb").read())
-image = cv2.imread(args["upload"])
-distance = args["distance"]
-
-# compute the hash for the upload and convert it
-queryHash = hash_distance(image)
-queryHash = convert_hash(queryHash)
-
-# search
-start = time.time()
-results = tree.get_all_in_range(queryHash, distance)
-results = sorted(results)
-end = time.time()
-print("Search Time =", (end - start))
-
-# display results
-i = 0
-similars = []
-for (d, h) in results:
-	resultPaths = hashes.get(h, [])
-	for resultPath in resultPaths:
-		if i < 10:
-			result = cv2.imread(resultPath)
-			print("Result Location:", resultPath)
-			result = cv2.resize(result, (600, 400))
-			if i < 1:
-				cv2.imshow(resultPath, result)
-			else:
-				similars.append(result)
-			i += 1
-
-if len(similars) > 0:
-	montage = build_montages(similars, (128, 128), (3, 3))[0]
-	cv2.imshow("Similar Images", montage)
-cv2.waitKey(0)
+import sys
+import os
 
 
+def search(tree, query, dist=30):
+	start = time.time()
+	results = sorted(tree.get_all_in_range(query, dist))
+	end = time.time()
+	print(f"Found {len(results)} results in {end - start} seconds")
+	return results
+
+
+def get_imgs(search_results, hashes):
+	img_paths = []
+	for point, idx in search_results: # O(len(search_results))
+		loc = hashes.get(idx, [])
+		for img_path in loc: # O(1) unless duplicate images at this index
+			img_paths.append(img_path)
+	return img_paths
+
+
+def display(img_paths):
+	img_path = img_paths.pop(0)
+	print(f"Best Match: {img_path}")
+	cv2.imshow(img_path, imutils.resize(cv2.imread(img_path, 500)))
+	if len(img_paths) > 0:
+		similars = [cv2.imread(i) for i in img_paths]
+		montage = build_montages(similars, (128, 128), (3, 3))[0]
+		cv2.imshow("Similar Images", montage)
+		print("Similar Images:")
+		for i in img_paths:
+			print(f" {i}")
+	cv2.waitKey(0)
+
+
+def check(img_path):
+	if not os.path.isfile(img_path):
+		sys.exit(1)
+	filename, ext = os.path.splitext(img_path)
+	if ext not in [".jpg", ".jpeg", ".png", ".bmp"]:
+		sys.exit(1)
+	return img_path
+
+
+if __name__ == "__main__":
+	ap = argparse.ArgumentParser()
+	ap.add_argument("-u", "--upload", required=True, type=str)
+	args = vars(ap.parse_args())
+	img_path = check(args["upload"])
+
+	tree = pickle.loads(open("vptree.pickle", "rb").read())
+	hashes = pickle.loads(open("hashes.pickle", "rb").read())
+
+	query_hash = hash_distance(img_path)
+	query_idx = convert_hash(query_hash)
+
+	search_results = search(tree, query_idx)
+	img_paths = get_imgs(search_results, hashes)
+	display(img_paths)
+
+
+
+##
